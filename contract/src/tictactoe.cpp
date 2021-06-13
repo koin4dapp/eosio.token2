33af0feb146e3b92599ca0a86f584588262dc42c
koin4dapp <koin4dapp@gmail.com>
2021-06-13 06:19:22 +0000

    Ok

--- contract/src/tictactoe.cpp
+++ contract/src/tictactoe.cpp
@@ -9,22 +9,22 @@ using namespace eosio;
 // The contract
 CONTRACT tictactoe : public contract {
 	public:
-  
+
 	using contract::contract;
 
 	tictactoe(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
-		hodl_symbol("SYS", 4) {} //singleton initialize    
+		hodl_symbol("SYS", 4) {} //singleton initialize
 
 	TABLE game_record {
 		name host;
 		name opponent;
 		eosio::asset hoststake;
 		eosio::asset opponentstake;
-	  	  
+		
 		std::vector<std::uint8_t> board{0,0,0,0,0,0,0,0,0};
 		name turn;
 		name winner;
-	  
+	
 		bool is_empty_cell(uint16_t row, uint16_t col) {return (board[row*3+col]==0);}
 		bool is_valid_movement(name by, uint16_t row, uint16_t col) {
 			if (winner==name() && by==turn && is_empty_cell(row, col) ) {
@@ -38,7 +38,7 @@ CONTRACT tictactoe : public contract {
 					board[row*3+col]=set;
 					turn=host;
 				}
-			  
+			
 				uint8_t test=((board[row*3+0]&board[row*3+1]&board[row*3+2]) 
 					|(board[0*3+col]&board[1*3+col]&board[2*3+col])
 					|(board[0]&board[4]&board[8])
@@ -52,18 +52,9 @@ CONTRACT tictactoe : public contract {
 			return false;
 		}
 
-		bool is_draw() {
-			uint8_t pos=0;
-			while (pos < 9) {
-			  if (board[pos]>0) break;
-			  pos++;			  
-			}
-			return (pos==9);
-		}
-
 		uint64_t primary_key() const {return host.value;}
 		uint128_t secondary_key() const {return combine_ids(host.value,opponent.value);}
-    };
+	};
 	
 	typedef eosio::multi_index<name("games"), game_record,
 		eosio::indexed_by<name("gameskey"), eosio::const_mem_fun<game_record, uint128_t, &game_record::secondary_key>>
@@ -76,7 +67,7 @@ CONTRACT tictactoe : public contract {
 	}
 
 	[[eosio::on_notify("eosio.token::transfer")]]
-    void create(name from, name to, eosio::asset quantity, std::string memo) {
+	void create(name from, name to, eosio::asset quantity, std::string memo) {
 		if (from == get_self() || to != get_self())
 			return;
 
@@ -84,10 +75,10 @@ CONTRACT tictactoe : public contract {
 		check(quantity.symbol == hodl_symbol, "These are not the droids you are looking for.");
  
 		name opponent = name(memo);
-	  
+	
 		check(is_account(opponent),"opponent account not found");
 		check(from!=opponent, "Could not challenge youself!");
-        
+
 		game_index _game(get_self(), get_self().value);
 		//find using secondary key
 		auto _gameskey = _game.get_index<name("gameskey")>(); //set secondary key
@@ -95,28 +86,28 @@ CONTRACT tictactoe : public contract {
 		auto itrc = _gameskey.find(combine_ids(opponent.value, from.value));
 		if (itrh==_gameskey.end() && itrc==_gameskey.end()) {
 			//ram charge to action caller
-			_game.emplace(get_self(), [&](auto& game) { 
-				game.host = from;
-				game.opponent = opponent;
-				game.hoststake = quantity;
-				game.opponentstake = asset(0.0000,hodl_symbol);
+			_game.emplace(get_self(), [&](auto& pair) { 
+				pair.host = from;
+				pair.opponent = opponent;
+				pair.hoststake = quantity;
+				pair.opponentstake = asset(0.0000,hodl_symbol);
 			});
 		}
 		else if (itrh!=_gameskey.end()) {
 			//ram charge to same_payer
-			_game.modify(*itrh, same_payer, [&](auto& game) { 
-				game.hoststake += quantity;
-			});  
+			_game.modify(*itrh, same_payer, [&](auto& pair) { 
+				pair.hoststake += quantity;
+			});
 		}
 		else {
 			//ram charge to same_payer
-			_game.modify(*itrc, same_payer, [&](auto& game) { 
-				game.opponentstake += quantity;
-			});  		  
+			_game.modify(*itrc, same_payer, [&](auto& pair) { 
+				pair.opponentstake += quantity;
+			});		
 		}
-    }
+	}
 	
-    ACTION move(name opponent, name host, name by, uint16_t row, uint16_t col) {
+	ACTION move(name opponent, name host, name by, uint16_t row, uint16_t col) {
 		check(has_auth(by), "please auth before!");
 		check(by==opponent || by==host, "only opponent or host can move!");
 		check(row<3 && col < 3, "invalid row or col!");
@@ -127,44 +118,54 @@ CONTRACT tictactoe : public contract {
 		check(itr->winner==name(), "game over!");
 		_game.modify(*itr, same_payer, [&]( auto& game ) {
 			check(game.is_valid_movement(by, row, col), "invalid move.");
-			if (game.winner!=name()) {
-				payback(game.winner, game.hoststake+game.opponentstake, "You got the prize.");
-			}
-			else if (game.is_draw()) {
-				payback(game.host, game.hoststake, "Stake refund.");
-				payback(game.opponent, game.opponentstake, "Stake refund.");
-			}	 
 		});
+		if (itr->winner!=name()) {
+			payback(itr->winner, itr->hoststake+itr->opponentstake, "You got the prize.");
+		}
+		else if (is_draw(itr->board)) {
+			payback(itr->host, itr->hoststake, "Stake refund.");
+			payback(itr->opponent, itr->opponentstake, "Stake refund.");
+		}
 	}
 	
-  ACTION close(name opponent, name host) {
-      //find by host (primary key)
-      check(has_auth(host), "Please auth yourself.");
-      game_index _game(get_self(), get_self().value);
-      //find using secondary key
-      auto _gameskey = _game.get_index<name("gameskey")>();
-      auto itr = _gameskey.find(combine_ids(host.value, opponent.value)); 
-      if (itr!=_gameskey.end()) {
-        if (bool& !(itr->is_draw())) {
-          if (itr->winner==name()) {
-            check(itr->opponentstake==asset(0,hodl_symbol), "Close failed, opponent has staked."); 
-            payback(itr->host, itr->hoststake, "Your stake freed.");
-          }
-        }
-      _gameskey.erase(itr);
-      }
-      else {
-        check(false, "Game not found.");
-      }
-  }   
+	ACTION close(name opponent, name host) {
+		//find by host (primary key)
+		check(has_auth(host), "Please auth yourself.");
+		game_index _game(get_self(), get_self().value);
+		//find using secondary key
+		auto _gameskey = _game.get_index<name("gameskey")>();
+		auto itr = _gameskey.find(combine_ids(host.value, opponent.value));
+		if (itr!=_gameskey.end()) {
+			if (!is_draw(itr->board)) {
+				if (itr->winner==name()) {
+					check(itr->opponentstake==asset(0,hodl_symbol), "Close failed, opponent has staked."); 
+					payback(itr->host, itrh->hoststake, "Your stake freed.");
+				}
+			}
+			_gameskey.erase(itr);
+			}
+		else {
+			check(false, "Game not found.");
+		}
+	}
 
-  private: 
+	private: 
+	
 	const symbol hodl_symbol;
-  
+
 	// concatenation of ids example
 	static uint128_t combine_ids(const uint64_t &x, const uint64_t &y) {
 		return (uint128_t{x} << 64) | y;
 	}
+
+	bool is_draw(std::vector<std::uint8_t> board) {
+		uint8_t pos=0;
+		while (pos < 9) {
+			if (board[pos]>0) break;
+			pos++;			
+		}
+		return pos==9;
+	}	
 	
 	void payback(name to, asset quantity, std::string memo) {
 		action{
